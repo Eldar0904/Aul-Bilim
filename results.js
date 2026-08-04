5befa79f19f9e6c90a9cdf6ce307c855d275ebdb
Eldar0904 <j226nktk7k@privaterelay.appleid.com>
2026-08-04 18:23:46 +0500

    Remove cabinet names from results carousel

--- results.js
+++ results.js
@@ -1,11 +1,6 @@
 (function () {
-  var subjects = {
-    kk: ['Физика кабинеті', 'Химия кабинеті', 'Биология кабинеті'],
-    ru: ['Кабинет физики', 'Кабинет химии', 'Кабинет биологии']
-  };
   var slides = Array.prototype.slice.call(document.querySelectorAll('.compare-slide'));
   var dots = Array.prototype.slice.call(document.querySelectorAll('.compare-dot'));
-  var label = document.querySelector('.compare-subject');
   var current = 0;
   var timer;
 
@@ -21,16 +16,12 @@
     current = index;
     slides.forEach(function (slide) { slide.classList.toggle('is-active', Number(slide.dataset.index) === index); });
     dots.forEach(function (dot) { dot.classList.toggle('is-active', Number(dot.dataset.index) === index); });
-    if (label) {
-      var lang = document.documentElement.getAttribute('data-lang') === 'ru' ? 'ru' : 'kk';
-      label.innerHTML = '<span lang="kk">' + subjects.kk[index] + '</span><span lang="ru">' + subjects.ru[index] + '</span>';
-    }
     updatePlaceholders();
   }
 
   function restart() {
     window.clearInterval(timer);
-    timer = window.setInterval(function () { show((current + 1) % subjects.kk.length); }, 4000);
+    timer = window.setInterval(function () { show((current + 1) % dots.length); }, 4000);
   }
 
   dots.forEach(function (dot) {
